3dee6a9b8a4f3cdc01dce54f7458cdef12d148c1
nikolaswise <nikolaswise@gmail.com>
2023-07-05 11:35:19 -0700

    most basic app possible

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,50 @@
 <script>
-  import Greet from '../lib/Greet.svelte'
+  import { version } from '$app/environment';
 </script>
 
-<h1>Welcome to Stucco</h1>
-<Greet />
 
+<main>
+  <h1>Stucco TL</h1>
+  <p>Write and publish structured data vocabularies.</p>
+  <p>
+    <span>{version}</span>
+  </p>
+</main>
 
+<style>
+
+html {
+  --bg-color: #fafafa;
+  --txt-color: #2a2a2e;
+  --code-bg: #eee;
+  --pink: rgb(249,118,126);
+  font-size: clamp(18px, 1.7vw, 28px);
+}
+
+.invert {
+  --bg-color: #191919;
+  --txt-color: #fafafa;
+  --pink: rgb(235, 181, 190);
+  --code-bg: #2a2a2e;
+}
+
+@media screen and (prefers-color-scheme: dark) {
+  html {
+    --bg-color: #191919;
+    --txt-color: #fafafa;
+    --code-bg: #2a2a2e;
+    --pink: rgb(235, 181, 190);
+    font-size: clamp(18px, 1.7vw, 28px);
+  }
+  .invert {
+    --bg-color: #fafafa;
+    --txt-color: #2a2a2e;
+    --pink: rgb(249,118,126);
+    --code-bg: #eee;
+  }
+}
+
+  main {
+    text-align: center;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,12 @@
 import adapter from '@sveltejs/adapter-static' // This was changed from adapter-auto
 import preprocess from 'svelte-preprocess'
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+
+const file = fileURLToPath(new URL('package.json', import.meta.url))
+const json = readFileSync(file, 'utf8')
+const pkg = JSON.parse(json)
+console.log(pkg.version)
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +16,9 @@ const config = {
 
   kit: {
     adapter: adapter(),
+    version: {
+		  name: pkg.version
+		}
   },
 }
 
